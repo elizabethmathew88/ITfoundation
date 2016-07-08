c7ed962b271a43ab1ade7cedf6b4c30bd3acd8a8
Elizabeth <elizabethmathew02@gmail.com>
2016-07-08 12:19:44 -0700

    Homework3

---
+++ Homework3/Homework3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the no of seconds");
+        string seconds = Console.ReadLine();
+        int sec = int.Parse(seconds);
+
+        int days = 0;
+        int hours = 0;
+        int mins = 0;
+        
+
+        int secperday = 86400;
+        int secperhour = 3600;
+        int secpermin = 60;
+
+        int secrem = sec;
+        while (secrem >= secperday)
+        {
+            days++;
+            secrem = secrem - secperday;
+        }
+        while (secrem >= secperhour)
+        {
+            hours++;
+            secrem = secrem - secperhour;
+        }
+        while (secrem >= secpermin)
+        {
+            mins++;
+            secrem = secrem - secpermin;
+        }
+
+
+        Console.WriteLine("{0}days {1}hours {2}mins {3}seconds", days, hours, mins, secrem);
+        Console.ReadLine();
+
+    }
+}
