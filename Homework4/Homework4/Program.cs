92c1803a40da4b512e21f206a15d40843017c59c
Elizabeth <elizabethmathew02@gmail.com>
2016-07-15 18:09:48 -0700

    Homework4

---
+++ Homework4/Homework4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Homework4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter a number: ");
+            string num = Console.ReadLine();
+            int x = int.Parse(num);
+            int i;
+            int k;
+
+            for (k = 1; k <=x; k++)
+            {
+                int fact = 1;
+                for (i = 0; i < k; i++)
+                {
+
+                    fact = fact * (k - i);
+                    
+                }
+                Console.WriteLine(" {0}! = {1}", k, fact);
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
