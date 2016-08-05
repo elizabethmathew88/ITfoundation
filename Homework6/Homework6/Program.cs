95be7112008c69292c2e7c570d268eb80fa5a417
Elizabeth <elizabethmathew02@gmail.com>
2016-08-05 12:48:55 -0700

    Homework6

---
+++ Homework6/Homework6/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+
+namespace Homework6
+{
+    class Television
+    {
+        private int channel = 0;
+        private int volume = 0;
+        private bool isOn = false;
+
+        public bool IsOn
+        {
+            get
+            {
+                return isOn;
+            }
+            set
+            {
+                isOn = value;
+
+            }
+        }
+
+
+        public int Channel
+        {
+            get
+            {
+                return channel;
+            }
+            set
+            {
+                channel = value;
+            }
+        }
+        public int Volume
+        {
+            get
+            {
+                return volume;
+            }
+            set
+            {
+                volume = value;
+            }
+        }
+
+    }
+
+
+
+    class TestTV
+    {
+        static void Main()
+        {
+            Television tv = new Television();
+
+
+            if (!tv.IsOn)//getting
+            {
+                tv.IsOn = true;
+            }
+
+            tv.Channel = 3;
+
+            tv.Volume++;
+            tv.Volume++;
+            tv.Volume++;
+            tv.Volume++;
+
+            tv.IsOn = false;
+
+        }
+    }
+}
